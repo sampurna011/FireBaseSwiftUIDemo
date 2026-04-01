91b415eebc777705f44d3cb60dc286d461869483
Sampurna Mishra <sampurna.mishra011@gmail.com>
2026-04-01 23:39:26 +0530

    Fire base SDK initial setup

---
+++ FirebaseDemo/FirebaseDemo/FirebaseDemoApp.swift
@@ -0,0 +1,37 @@
+//
+//  FirebaseDemoApp.swift
+//  FirebaseDemo
+//
+//  Created by Deepu mishra on 01/04/26.
+//
+
+import SwiftUI
+import FirebaseCore
+
+@main
+struct FirebaseDemoApp: App {
+    
+   // @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        FirebaseApp.configure()
+        print("FirebaseApp configure")
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
